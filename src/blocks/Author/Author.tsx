6cb5f8743109f10bbd622e83ee6ad11ccc7abc3d
Minh Nguyen <minhhien134@gmail.com>
2025-07-02 16:55:42 +0700

    fix(Author): replace MediaItem with Image

--- src/blocks/Author/Author.tsx
+++ src/blocks/Author/Author.tsx
@@ -1,15 +1,18 @@
-import { MediaItem } from '../../elements/MediaItem/MediaItem';
+import { Image } from '../../elements/Image/Image';
 import { ExpertType } from '../../types';
 
 export const Author: React.FC<{ author: ExpertType }> = ({ author }) => (
   <div className="group flex gap-2 items-center">
-    <div className="w-11 h-11">
-      <MediaItem
-        data={author.profilePicture}
-        aspectRatio="square"
-        rounded="full"
-      />
-    </div>
+    {/* {author.profilePicture && ( */}
+      <div className="w-11 h-11">
+        <Image
+          data={author.profilePicture}
+          aspectRatio="square"
+          rounded="full"
+          fallbackSrc='https://ui-avatars.com/api/?name=User&background=cccccc&color=ffffff&size=250'
+        />
+      </div>
+    {/* )} */}
     <div className="flex flex-col gap-1">
       <div className="font-heading font-medium text-lg dark:text-slate-100">
         {author.name}
