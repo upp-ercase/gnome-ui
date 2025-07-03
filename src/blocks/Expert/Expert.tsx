29dda61e9bcbc95d05354d5e72bce41c0a59c58a
Minh Nguyen <minhhien134@gmail.com>
2025-07-03 14:01:11 +0700

    fix(Expert): modify

--- src/blocks/Expert/Expert.tsx
+++ src/blocks/Expert/Expert.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { ExpertType } from '../../types';
-import { MediaItem } from '../../elements/MediaItem/MediaItem';
+import { Image } from '../../elements/Image/Image';
 
 export const Expert: React.FC<{
   data: ExpertType;
@@ -23,7 +23,7 @@ export const Expert: React.FC<{
           'justify-end': alignment === 'end',
         })}
       >
-        <MediaItem data={profilePicture} aspectRatio="square" />
+        <Image data={profilePicture} aspectRatio="square" />
         {description && (
           <div
             className={classNames(
@@ -36,7 +36,7 @@ export const Expert: React.FC<{
       </div>
       <div
         className={classNames(
-          'w-full px-4 pt-4 pb-6 flex flex-col gap-1 rounded-b-theme',
+          'w-full px-4 pt-3 pb-6 flex flex-col rounded-b-theme',
           {
             'text-center': alignment === 'center',
             'text-end': alignment === 'end',
@@ -53,25 +53,10 @@ export const Expert: React.FC<{
         {role && (
           <div
             className={classNames(
-              'text-slate-600 dark:text-slate-100/80 inverse:text-slate-100/80',
+              'mt-1 text-slate-600 dark:text-slate-100/80 inverse:text-slate-100/80',
             )}
           >
             {role}
-          </div>
-        )}
-        {specialization && (
-          <div
-            className={classNames(
-              'flex flex-wrap gap-2 text-slate-600 dark:text-slate-100/80 inverse:text-slate-100/80',
-              {
-                'justify-center': alignment === 'center',
-                'justify-end': alignment === 'end',
-              },
-            )}
-          >
-            {specialization.map((item, index) => (
-              <span key={index}>{item}</span>
-            ))}
           </div>
         )}
         {organization && (
@@ -83,6 +68,22 @@ export const Expert: React.FC<{
             {organization}
           </div>
         )}
+        {specialization && (
+          <div
+            className={classNames(
+              'mt-2 flex flex-wrap gap-2 text-slate-600 dark:text-slate-100/80 inverse:text-slate-100/80',
+              {
+                'justify-center': alignment === 'center',
+                'justify-end': alignment === 'end',
+              },
+            )}
+          >
+            {specialization.map((item, index) => (
+              <span key={index} className="uppercase text-xs">{item}{index < specialization.length - 1 && ","}</span>
+            ))}
+          </div>
+        )}
+        
       </div>
     </div>
   );
