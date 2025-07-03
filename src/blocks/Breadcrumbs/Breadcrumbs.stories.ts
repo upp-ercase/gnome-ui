ba60f9e2608f60396f845e44b9747364275782b1
Minh Nguyen <minhhien134@gmail.com>
2025-07-03 10:42:49 +0700

    docs(Breadcrumbs): add Storybook stories

---
+++ src/blocks/Breadcrumbs/Breadcrumbs.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Breadcrumbs } from './Breadcrumbs';
+
+const meta = {
+  title: 'Blocks/Breadcrumbs',  
+  component: Breadcrumbs,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Breadcrumbs>;
+
+export default meta;
+
+type Story = StoryObj<typeof Breadcrumbs>;
+
+const baseBreadcrumbs = [
+    {
+      label: "Home",
+      href: "/"
+    },
+    {
+      label: "Blog",
+      href: "/blog"
+    },
+    {
+      label: "Guide",
+      href: "/blog/category/guide"
+    },
+  ]
+
+export const Default: Story = {
+  args: {
+    paths: baseBreadcrumbs
+  }
+}
