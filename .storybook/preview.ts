79bdbf4f3eff22a725e40e76c1431819fa125905
Minh Nguyen <minhhien134@gmail.com>
2025-07-07 15:52:29 +0700

    chore: config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,12 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from '@storybook/react-vite';
 import '../src/styles/globals.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
