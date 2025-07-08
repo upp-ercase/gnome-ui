335226565125bf1c3662e0a597f60123adf0aa1c
Minh Nguyen <minhhien134@gmail.com>
2025-07-08 14:55:09 +0700

    chore: update dependencies and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,25 @@ module.exports = {
     './.storybook/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.stories.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      spacing: {
+        1.25: '0.3125rem',
+        2.5: '0.625rem',
+        4.5: '1.125rem',
+        7.5: '1.875rem',
+        13: '3.25rem',
+        14: '3.5rem',
+        15: '3.75rem',
+        18: '4.5rem',
+        19.5: '4.875rem',
+        26: '6.5rem',
+        39: '9.75rem',
+        42: '10.5rem',
+        63: '15.75rem',
+      },
+    }
+  },
   plugins: [
     require('@tailwindcss/typography'),
     plugin(function ({ addVariant }) {
