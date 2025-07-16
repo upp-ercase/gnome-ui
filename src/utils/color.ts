cad46ce9f966720185f4b1d3d9eac0585a1ced0f
Minh Nguyen <minhhien134@gmail.com>
2025-07-16 13:38:59 +0700

    chore: add new utils

---
+++ src/utils/color.ts
@@ -0,0 +1,4 @@
+
+export function convertHextoRgb(hex: string) { 
+	return `${parseInt(hex.slice(1, 3), 16)}, ${parseInt(hex.slice(3, 5), 16)}, ${parseInt(hex.slice(5, 7), 16)}`
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './font';
 export * from './form';
 export * from './class';
 export * from './string';
+export * from './color';
