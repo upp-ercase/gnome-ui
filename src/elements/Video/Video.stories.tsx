d4d0a173363257c8a1094327c26ee7d5559b5249
Minh Nguyen <minhhien134@gmail.com>
2025-07-18 12:45:21 +0700

    chore(storybook): add decorators to Videos

--- src/elements/Video/Video.stories.tsx
+++ src/elements/Video/Video.stories.tsx
@@ -4,6 +4,13 @@ import { Video } from './Video';
 const meta = {
   title: 'Elements/Video',
   component: Video,
+  decorators: [
+    (Story) => (
+      <div className="max-w-xl">
+        <Story />
+      </div>
+    ),
+  ],
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
@@ -14,7 +21,7 @@ export default meta;
 
 type Story = StoryObj<typeof Video>;
 
-const video = {
+const mockVideo = {
   url: 'https://videos.ctfassets.net/ehmycbmc25mu/1fqCODzYo0GjjHUG3WOXwa/86e28c6b7cccd55f5794b6f74c0c2878/corporation_teamwork.mp4',
   title: 'Teamwork',
   description: '',
@@ -24,49 +31,49 @@ const video = {
 
 export const AspectRatioAuto: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     aspectRatio: 'auto',
   },
 };
 
 export const AspectRatioSquare: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     aspectRatio: 'square',
   },
 };
 
 export const AspectRatio16x9: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     aspectRatio: '16/9',
   },
 };
 
 export const AspectRatio4x3: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     aspectRatio: '4/3',
   },
 };
 
 export const AspectRatio3x2: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     aspectRatio: '3/2',
   },
 };
 
 export const VideoAutoplay: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     videoAutoplay: true,
   },
 };
 
 export const ZoomInOverHover: Story = {
   args: {
-    data: video,
+    data: mockVideo,
     videoAutoplay: true,
     zoomInOnHover: true,
   },
