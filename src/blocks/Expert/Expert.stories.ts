96038c836b7a082e135c82a9e618a76068c4dc86
Minh Nguyen <minhhien134@gmail.com>
2025-07-18 12:48:07 +0700

    refactor: Expert.stories.ts

--- src/blocks/Expert/Expert.stories.ts
+++ src/blocks/Expert/Expert.stories.ts
@@ -15,50 +15,34 @@ export default meta;
 
 type Story = StoryObj<typeof Expert>;
 
-const mockProfilePicture = {
-  id: '1',
-  url: 'https://randomuser.me/api/portraits/men/32.jpg',
-  title: 'John Doe',
-  description: 'Profile picture of John Doe',
-  width: 250,
-  height: 250,
-  contentType: 'image/jpeg',
-};
-
-const baseExpert: ExpertType = {
+const mockExpert: ExpertType = {
   name: 'John Doe',
-  profilePicture: mockProfilePicture,
+  profilePicture: {
+    url: 'https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=70&w=250&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'John Doe',
+    description: 'Profile picture of John Doe',
+    width: 250,
+    height: 250,
+    contentType: 'image/jpeg',
+  },
   role: 'Senior Writer',
   description: 'John is a senior writer with 10 years of experience.',
   specialization: ['Technology', 'Science'],
   organization: 'Webbie Media',
-  alignment: 'center',
+  alignment: 'start',
   contentType: 'expert',
 };
 
 export const Start: Story = {
   args: {
-    data: {
-      name: 'John Doe',
-      profilePicture: mockProfilePicture,
-      role: 'Senior Writer',
-      description: 'John is a senior writer with 10 years of experience.',
-      specialization: ['Technology', 'Science'],
-      organization: 'Webbie Media',
-      alignment: 'start',
-    },
+    data: mockExpert,
   },
 };
 
 export const Center: Story = {
   args: {
     data: {
-      name: 'John Doe',
-      profilePicture: mockProfilePicture,
-      role: 'Senior Writer',
-      description: 'John is a senior writer with 10 years of experience.',
-      specialization: ['Technology', 'Science'],
-      organization: 'Webbie Media',
+      ...mockExpert,
       alignment: 'center',
     },
   },
@@ -67,12 +51,7 @@ export const Center: Story = {
 export const End: Story = {
   args: {
     data: {
-      name: 'John Doe',
-      profilePicture: mockProfilePicture,
-      role: 'Senior Writer',
-      description: 'John is a senior writer with 10 years of experience.',
-      specialization: ['Technology', 'Science'],
-      organization: 'Webbie Media',
+      ...mockExpert,
       alignment: 'end',
     },
   },
